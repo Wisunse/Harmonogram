a11bb7075179ca27147454f2eecd74bfd2b80ead
Wisunse <mateusz.mydlarz@protonmail.com>
2017-09-07 11:05:31 +0200

    change in management

--- public/scripts/controllers/management.controller.js
+++ public/scripts/controllers/management.controller.js
@@ -17,50 +17,54 @@ angular.module('Harmonogram')
                 return new Array(num);
         };
 
-        $scope.colorBricks = function() {
-
-            if(management.wholeDate.year !== null && cars.cars !== null && management.registry !== null && management.daysInMonth !== null) {
-
-                var year = management.wholeDate.year;
-                var month = management.wholeDate.month;
-                $scope.managementArray = [];
-
-                cars.cars.forEach(function(car) {
-
-                    var obj = { car_id: car.id, car_name: car.name,  days: [ ] };
-
-                    for(var day = 1; day<management.daysInMonth+1; day++) {
-
-                        var stringDate = year+'-'+month+'-'+day;
-                        var date = new Date(stringDate);
-                        var matchingReg = null;
-                        management.registry.forEach(function(reg) {
-
-                            var dataStart = new Date(reg.data_start);
-                            var dataEnd = new Date(reg.data_end);
-
-                            if(reg.cars_id == car.id && date >= dataStart && date <= dataEnd ) {
-
-                                matchingReg = reg;
-                            }
-
-                        });
-                        var day_obj = { day_number: day, reg_id: null, data_start: null, data_end: null };
-                        if(matchingReg!== null){
-                            day_obj.reg_id = matchingReg.id;
-                            day_obj.data_start = matchingReg.data_start;
-                            day_obj.data_end = matchingReg.data_end;
-                        }
-                        obj.days.push(day_obj);
-
-                    }
-                    $timeout(function() {$scope.$apply(function () {$scope.managementArray.push(obj)})});
-                });
-                console.log($scope.managementArray);
-            } else {
-                setTimeout(function() { $scope.colorBricks() }, 1000);
-            }
-        };$scope.colorBricks();
+        // $scope.colorBricks = function() {
+        //
+        //     if(management.wholeDate.year !== null && cars.cars !== null && management.registry !== null && management.daysInMonth !== null) {
+        //
+        //         var year = management.wholeDate.year;
+        //         var month = management.wholeDate.month;
+        //         $scope.managementArray = [];
+        //
+        //         cars.cars.forEach(function(car) {
+        //
+        //             var obj = { car_id: car.id, car_name: car.name,  days: [ ] };
+        //
+        //             for(var day = 1; day<management.daysInMonth+1; day++) {
+        //
+        //                 var stringDate = year+'-'+month+'-'+day;
+        //                 var date = new Date(stringDate);
+        //                 var matchingReg = null;
+        //                 management.registry.forEach(function(reg) {
+        //
+        //                     var dataStart = new Date(reg.data_start);
+        //                     var dataEnd = new Date(reg.data_end);
+        //
+        //                     if(reg.cars_id == car.id && date >= dataStart && date <= dataEnd ) {
+        //
+        //                         matchingReg = reg;
+        //                     }
+        //
+        //                 });
+        //                 var day_obj = { day_number: day, reg_id: null, data_start: null, data_end: null };
+        //                 if(matchingReg!== null){
+        //                     day_obj.reg_id = matchingReg.id;
+        //                     day_obj.data_start = matchingReg.data_start;
+        //                     day_obj.data_end = matchingReg.data_end;
+        //                 }
+        //                 obj.days.push(day_obj);
+        //
+        //             }
+        //             $timeout(function() {
+        //                 $scope.$apply(function () {
+        //                     $scope.managementArray.push(obj)
+        //                 })
+        //             },500);
+        //         });
+        //         console.log($scope.managementArray);
+        //     } else {
+        //         setTimeout(function() { $scope.colorBricks() }, 1000);
+        //     }
+        // };$scope.colorBricks();
 
         $scope.showDayDetails = function(day) {
             console.log(day);
@@ -75,7 +79,7 @@ angular.module('Harmonogram')
                     $http.get('/all_registry').then(function successCallback(response) {
                         var data = response.data;
                         management.registry = data.all_registry;
-                        $scope.colorBricks();
+                        management.colorBricks();
                         management.closeDialog();
                     });
 
--- public/scripts/services/management.service.js
+++ public/scripts/services/management.service.js
@@ -74,50 +74,50 @@ angular.module('Harmonogram')
 
     };
 
-        // factory.colorBricks = function() {
-        //
-        //     if(factory.wholeDate.year !== null && cars.cars !== null && factory.registry !== null && factory.daysInMonth !== null) {
-        //
-        //         var year = factory.wholeDate.year;
-        //         var month = factory.wholeDate.month;
-        //         factory.managementArray = [];
-        //
-        //         cars.cars.forEach(function(car) {
-        //
-        //             var obj = { car_id: car.id, car_name: car.name,  days: [ ] };
-        //
-        //             for(var day = 1; day<factory.daysInMonth+1; day++) {
-        //
-        //                 var stringDate = year+'-'+month+'-'+day;
-        //                 var date = new Date(stringDate);
-        //                 var matchingReg = null;
-        //                 factory.registry.forEach(function(reg) {
-        //
-        //                     var dataStart = new Date(reg.data_start);
-        //                     var dataEnd = new Date(reg.data_end);
-        //
-        //                     if(reg.cars_id == car.id && date >= dataStart && date <= dataEnd ) {
-        //                         matchingReg = reg;
-        //                     }
-        //
-        //                 });
-        //                 var day_obj = { day_number: day, reg_id: null, data_start: null, data_end: null };
-        //                 if(matchingReg!== null){
-        //                     day_obj.reg_id = matchingReg.id;
-        //                     day_obj.data_start = matchingReg.data_start;
-        //                     day_obj.data_end = matchingReg.data_end;
-        //                 }
-        //                 obj.days.push(day_obj);
-        //
-        //             }
-        //             factory.managementArray.push(obj);
-        //             console.log(factory.managementArray);
-        //         });
-        //
-        //     } else {
-        //         setTimeout(function() { factory.colorBricks() }, 1000);
-        //     }
-        // };factory.colorBricks();
+        factory.colorBricks = function() {
+
+            if(factory.wholeDate.year !== null && cars.cars !== null && factory.registry !== null && factory.daysInMonth !== null) {
+
+                var year = factory.wholeDate.year;
+                var month = factory.wholeDate.month;
+                factory.managementArray = [];
+
+                cars.cars.forEach(function(car) {
+
+                    var obj = { car_id: car.id, car_name: car.name,  days: [ ] };
+
+                    for(var day = 1; day<factory.daysInMonth+1; day++) {
+
+                        var stringDate = year+'-'+month+'-'+day;
+                        var date = new Date(stringDate);
+                        var matchingReg = null;
+                        factory.registry.forEach(function(reg) {
+
+                            var dataStart = new Date(reg.data_start);
+                            var dataEnd = new Date(reg.data_end);
+
+                            if(reg.cars_id == car.id && date >= dataStart && date <= dataEnd ) {
+                                matchingReg = reg;
+                            }
+
+                        });
+                        var day_obj = { day_number: day, reg_id: null, data_start: null, data_end: null };
+                        if(matchingReg!== null){
+                            day_obj.reg_id = matchingReg.id;
+                            day_obj.data_start = matchingReg.data_start;
+                            day_obj.data_end = matchingReg.data_end;
+                        }
+                        obj.days.push(day_obj);
+
+                    }
+                    factory.managementArray.push(obj);
+                    console.log(factory.managementArray);
+                });
+
+            } else {
+                setTimeout(function() { factory.colorBricks() }, 1000);
+            }
+        };factory.colorBricks() ;
 
 
 
